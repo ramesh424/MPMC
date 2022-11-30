6b0330f70a8c5dbb63cf39c1833e781979a71071
D Ramesh Reddy <mail2donapati@gmail.com>
2022-11-30 14:41:08 +0530

    Add files via upload

---
+++ ARM/asmd.s
@@ -0,0 +1,55 @@
+;The semicolon is used to lead an inline documentation.
+;This is the first ARM Assembly language program you see in the lab.
+;This program skeleton was from Dave Duguid and Trevor Douglas in summer 2013.
+;When you write your program, you could have your info at the top document block.
+;For Example:  Your Name, Student Number, what the program is for, and what it does etc.
+
+;;; Directives
+STACK_ADDR_123G EQU 0x20008000
+          PRESERVE8
+          THUMB       
+ 
+; Vector Table Mapped to Address 0 at Reset
+; Linker requires __Vectors to be exported
+ 
+          AREA    RESET, DATA, READONLY
+          EXPORT  __Vectors
+ 
+__Vectors 
+	  DCD  0x20001000     ; stack pointer value when stack is empty
+			;The processor uses a full descending stack. 
+			;This means the stack pointer holds the address of the last 
+			;stacked item in memory. When the processor pushes a new item 
+			;onto the stack, it decrements the stack pointer and then 
+			;writes the item to the new memory location.
+
+          DCD  Reset_Handler  ; reset vector
+  
+          ALIGN
+ 
+; The program
+; Linker requires Reset_Handler
+ 
+          AREA    MYCODE, CODE, READONLY
+ 
+   	  ENTRY
+   	  EXPORT Reset_Handler
+ 
+ 
+Reset_Handler
+;;;;;;;;;;User Code Starts from the next line;;;;;;;;;;;;
+
+   	  MOV R0, #4
+	  MOV R1, #2
+ 
+
+	  ADD R3, R1, R0
+	  MUL R2,R1,R0
+	  SUB R4,R0,R1
+
+	  UDIV R5,R0,R1
+	  
+START   
+      B  START
+ 
+          END	;End of the program
